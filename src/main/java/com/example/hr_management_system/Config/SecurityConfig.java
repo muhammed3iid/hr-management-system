1e9e409dfe88964dee063fdf3579f0fa8301e44d
muhammed3iid <muhammed3iid@gmail.com>
2024-07-18 22:48:20 +0300

    Keycloak authorization update

--- src/main/java/com/example/hr_management_system/Config/SecurityConfig.java
+++ src/main/java/com/example/hr_management_system/Config/SecurityConfig.java
@@ -3,27 +3,39 @@ package com.example.hr_management_system.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final JwtConverter jwtConverter;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) ->
-                authz.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                        .anyRequest().authenticated());
-
-        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)));
+        http
+//                Configuring Authorization
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/api/**").hasRole("HR")
+                        .anyRequest().authenticated()
+                )
+//                Configuring OAuth2 Resource Server
+                .oauth2ResourceServer(oauth2Configurer -> oauth2Configurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwt -> {
+                    Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+                    Map<String, Collection<String>> clientAccess = (Map<String, Collection<String>>) resourceAccess.get("crewper-client-id");
+                    Collection<String> roles = clientAccess.get("roles");
+                    var grantedAuthorities = roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                            .toList();
+                    return new JwtAuthenticationToken(jwt, grantedAuthorities);
+                })))
+        ;
 
         return http.build();
     }
